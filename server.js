4d73d427d4985618696d2b117f8aaadd80a5dd7b
Kota Nonaka <uutarou@innovation.local>
2017-05-16 08:30:54 +0900

    remove socket.io

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const app = require('express')()
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
 const path = require('path')
 
 app.get('/', (req,res) => {
@@ -9,11 +8,6 @@ app.get('/', (req,res) => {
 
 app.get('/comment.css', (req,res) => {
     res.sendFile(path.join(__dirname,'view','comment.css'))
-})
-
-io.on('connection', (socket) => {
-    console.log('user conected')
-
 })
 
 exports.runServer = () => {
