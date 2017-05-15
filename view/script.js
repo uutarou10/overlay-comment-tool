df7547f13dee1f61b3a146d24a465575a2f6ef2d
Kota Nonaka <uutarou@innovation.local>
2017-05-15 14:26:03 +0900

    コメントが動くように変更

--- view/script.js
+++ view/script.js
@@ -2,26 +2,28 @@ let commentArea = null
 
 document.addEventListener('DOMContentLoaded', () => {
     commentArea = document.getElementById('comment-area')
-    createKeyframes();
+    
+    for(i=0; i <=12; i+=1) {
+        createKeyframes(window.parent.screen.height / 12 * i, i)
+    }
 })
 
 let interval = window.setInterval(() => {
     let comment = document.createElement('span')
-    comment.setAttribute('class', 'comment')
+    comment.setAttribute('class', 'comment anime-' + (Math.floor(Math.random() * 10) + 1))
     comment.textContent = 'コメントだよ！'
     commentArea.appendChild(comment)
 
     deleteOutOfDisplayElement()
 },1000)
 
-const createKeyframes = () => {
+const createKeyframes = (y,index) => {
     const style = document.getElementsByTagName('style')[0]
     const windowWidth = window.parent.screen.width
     const windowHeight = window.parent.screen.height
 
-    const y = 30
     const keyframes =`
-        @keyframes anime {
+        @keyframes anime${index} {
             from{transform: translate(${windowWidth}px,${y}px);}
             to{transform: translate(${windowWidth * -1}px,${y}px);}
         }`
